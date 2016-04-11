4dea45356076d4e7cddf0c0f683e41959f8c18f1
Erwan Carriou <erwan.carriou@mac.com>
2016-04-11 19:58:59 +0200

    - fix a bug on HTML and node export

--- src/js/app.js
+++ src/js/app.js
@@ -2,5 +2,7 @@ var runtime = require('system-runtime');
 
 // import the system
 var systemId = runtime.require('db').system({{system}});
+// set the level of log
+runtime.require('logger').level('debug');
 // run the system
 runtime.require(systemId).main();
