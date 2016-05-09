d9a098da08cc41457f403f58922cb5f085db3faf
Erwan Carriou <erwan.carriou@mac.com>
2016-05-09 22:50:02 +0200

    - update jsPlumb - update ace

--- dist/designer/lib/ace/snippets/mipsassembler.js
+++ dist/designer/lib/ace/snippets/mipsassembler.js
@@ -1,1 +1,1 @@
-define("ace/snippets/mipsassembler",["require","exports","module"],function(e,t,n){"use strict";t.snippetText=undefined,t.scope=""})
+ace.define("ace/snippets/mipsassembler",["require","exports","module"],function(e,t,n){"use strict";t.snippetText=undefined,t.scope=""})
--- dist/designer/lib/ace/snippets/swift.js
+++ dist/designer/lib/ace/snippets/swift.js
@@ -1,1 +1,1 @@
-define("ace/snippets/swift",["require","exports","module"],function(e,t,n){"use strict";t.snippetText=undefined,t.scope="swift"})
+ace.define("ace/snippets/swift",["require","exports","module"],function(e,t,n){"use strict";t.snippetText=undefined,t.scope="swift"})
