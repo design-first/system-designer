7d169920207d1d29f2775a445b92963a45129857
Erwan Carriou <erwan.carriou@mac.com>
2016-10-09 18:55:51 +0200

    Improve Github push process

--- src/components/js/app-github.js
+++ src/components/js/app-github.js
@@ -1,8 +1,18 @@
+/* 
+ * @description {{description}}
+ * @module {{name}}
+ * @version {{version}}
+ * @requires system-runtime
+ */
+
+// install System Runtime
 var runtime = require('system-runtime');
 
 // install the system
 var systemId = runtime.install('{{name}}.json');
+
 // set the level of log
 runtime.require('logger').level('{{logLevel}}');
+
 // start the system
 runtime.start(systemId);
