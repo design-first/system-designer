083d5f33e922284bd4233c940365ed21b64530e1
Erwan Carriou <erwan.carriou@mac.com>
2017-04-24 22:36:05 +0200

    Behaviors are not overridden on composition (fix #77)

--- src/scripts/designer.js
+++ src/scripts/designer.js
@@ -761,9 +761,9 @@ runtime.on('ready', function ready() {
             id = _getSchemaId(behavior.component);
             def = schemas[id];
 
-            if (def && (def[behavior.state] === 'method' || def[behavior.state] === 'event')) {
-                result = !_existBehavior(behavior.component, behavior.state, behaviors);
-            }
+            //if (def && (def[behavior.state] === 'method' || def[behavior.state] === 'event')) {
+            //    result = !_existBehavior(behavior.component, behavior.state, behaviors);
+            //}
 
             if ((behavior.state === 'main' || behavior.state === 'start' || behavior.state === 'stop') && behavior.component === sysId) {
                 result = false;
