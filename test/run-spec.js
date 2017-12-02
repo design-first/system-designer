15b78960dc776f766c402e40e11aa681b347fce6
Erwan Carriou <erwan.carriou@mac.com>
2017-12-03 00:36:37 +0100

    update test

---
+++ test/run-spec.js
@@ -0,0 +1,28 @@
+describe('System Designer', function () {
+  it('can be started', async function (done) {
+    try {
+      const puppeteer = require('puppeteer');
+      const browser = await puppeteer.launch({
+        ignoreHTTPSErrors: true,
+        args: [
+          '--disable-setuid-sandbox',
+          '--no-sandbox',
+          '--allow-file-access-from-files'
+        ]
+      });
+      const page = await browser.newPage();
+
+      await page.goto('file://' + global.process.env.PWD + '/dist/index.html');
+      await page.content();
+      await page.waitForSelector('#myModalLabel');
+
+      const version = await page.$eval('#designer-menubar-actions', el => el.innerText);
+
+      await browser.close();
+      expect(version.trim()).toBe('v2.1.0');
+      done();
+    } catch (e) {
+      done.fail(err);
+    }
+  })
+});
