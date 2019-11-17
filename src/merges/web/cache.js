445c615411702e1bc8dd23b30e48c28e400b6b15
Erwan Carriou <erwan.carriou@mac.com>
2019-11-17 16:11:09 +0100

    Bump 4.3.0

--- src/merges/web/cache.js
+++ src/merges/web/cache.js
@@ -1,7 +1,7 @@
 // System Designer - Copyright 2019 Erwan Carriou
 // Licensed under the Apache License, Version 2.0 (the "License")
 
-const version = 'v4.2.0';
+const version = 'v4.3.0';
 
 const clearCaches = () => {
   return caches.keys().then(keys => {
