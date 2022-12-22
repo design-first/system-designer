ba81adb6873bd33fe48a17634a2d914b471be4ec
Erwan Carriou <erwan.carriou@mac.com>
2022-12-22 23:01:30 +0100

    chore: improve tests

--- cypress/e2e/common/system-designer.js
+++ cypress/e2e/common/system-designer.js
@@ -1,4 +1,29 @@
-import { Given, When, Then, Then as And } from '@badeball/cypress-cucumber-preprocessor'
+/*
+ * System Designer
+ *
+ * https://designfirst.io/systemdesigner/
+ *
+ * Copyright 2022 Erwan Carriou
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  Given,
+  When,
+  Then,
+  Then as And,
+} from '@badeball/cypress-cucumber-preprocessor'
 
 Given('Users have opened System Designer', () => {
   cy.visit('/').get('#designer-spaces-type').contains('Systems')
@@ -19,7 +44,6 @@ When('Users click to create a system', () => {
 
 And('Users enter system name as {string}', (systemname) => {
   cy.get('#designer-dialog-system-creation-modal')
-    .should('exist')
     .get('#designer-dialog-system-creation-name')
     .type(systemname)
 })
@@ -43,10 +67,7 @@ And('Users click to create a schema', () => {
 })
 
 And('Users enter schema name as {string}', (schemaname) => {
-  cy.get('#designer-dialog-schema-creation-name')
-    .should('exist')
-    .get('#designer-dialog-schema-creation-name')
-    .type(schemaname)
+  cy.get('#designer-dialog-schema-creation-name').type(schemaname)
 })
 
 And('Users click on schema create button', () => {
@@ -54,10 +75,11 @@ And('Users click on schema create button', () => {
 })
 
 Then('Users is able to see the schema {string}', (schemaname) => {
-  cy.get('.panel-title').should('be.visible').contains(schemaname)
+  cy.get('.panel-title').contains(schemaname).should('be.visible')
 })
 
 // Component
+
 When('Users click on component tab', () => {
   cy.get('#designer-menu-item-components').click()
 })
@@ -67,5 +89,5 @@ And('Users click to create a component', () => {
 })
 
 Then('Users is able to see the component', () => {
-  cy.get('.panel-title').should('exist')
+  cy.get('.panel-title').should('be.visible')
 })
