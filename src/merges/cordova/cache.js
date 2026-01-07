52555d846640e16c77435777aaaea6a793b0b203
Erwan Carriou <erwan.carriou@mac.com>
2026-01-07 17:25:04 +0100

    chore: update year in licence

--- src/merges/cordova/cache.js
+++ src/merges/cordova/cache.js
@@ -1,4 +1,4 @@
-// System Designer - Copyright 2024 Erwan Carriou
+// System Designer - Copyright 2026 Erwan Carriou
 // Licensed under the Apache License, Version 2.0 (the "License")
 
 const version = 'v5.4.0'
